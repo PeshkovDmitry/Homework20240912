880f937210e331c0ceefe3d2f014e7323ca05dca
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-09-14 07:36:52 +0300

    Решено задание 2

---
+++ Task02.py
@@ -0,0 +1,15 @@
+"""
+Задача 2. Работа с текущим временем и датой
+Напишите скрипт, который получает текущее время и дату, а затем выводит их в
+формате YYYY-MM-DD HH:MM:SS. Дополнительно, выведите день недели и номер
+недели в году.
+"""
+
+from datetime import datetime
+import locale
+
+
+if __name__ == "__main__":
+    locale.setlocale(locale.LC_ALL, ('ru_RU', 'UTF-8'))
+    current_time = datetime.now()
+    print(current_time.strftime( '%Y-%m-%d %H:%M:%S, %A, неделя %W'))
